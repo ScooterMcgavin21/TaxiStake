cfb93549dfb39feadcd32e93df09c5e732d6f4ea
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-09-15 15:15:33 -0600

    Added Navbar and elements to render a navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+
+function Navbar() {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>Logo</NavLogo>
+          <h1>Hey</h1>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+}
+
+export default Navbar;
