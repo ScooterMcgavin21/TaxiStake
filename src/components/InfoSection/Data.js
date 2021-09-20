3573a94e841e3c205693581b1407fd2d01583fa0
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-09-19 19:29:32 -0600

    added and styled multiple pages via Data infosection

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transactions with zero fees',
+  description: 'Get access to our app',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/1data.svg'),
+  alt: 'car',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Access',
+  headline: 'Unlimited Transactions with zero fees',
+  description: 'Get access to our app',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: require('../../images/2analyze.svg'),
+  alt: 'bank',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transactions with zero fees',
+  description: 'Get access to our app',
+  buttonLabel: 'Start now',
+  imgStart: false,
+  img: require('../../images/3bitcoin.svg'),
+  alt: 'paper',
+  dark: false,
+  primary: false,
+  darkText: true
+};
